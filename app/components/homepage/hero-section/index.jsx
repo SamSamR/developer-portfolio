4af291f83a875de8443f5927267e902f407d4d79
SamSamR <samroddenpro@gmail.com>
2024-04-11 18:02:01 -0400

    About Card Edit

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -100,61 +100,66 @@ function HeroSection() {
           <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
             <code className="font-mono text-xs md:text-sm lg:text-base">
               <div className="blink">
-                <span className="mr-2 text-pink-500">const</span>
-                <span className="mr-2 text-white">coder</span>
-                <span className="mr-2 text-pink-500">=</span>
+                <span className="mr-2 text-pink-500">public class </span>
+                <span className="mr-2 text-white">Coder</span>
+                <span className="mr-2 text-pink-500">()</span>
                 <span className="text-gray-400">{'{'}</span>
               </div>
               <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
-                <span className="text-gray-400">{`'`}</span>
+              <span className="ml-4 lg:ml-8 mr-2 text-pink-500">public string</span>
+                <span className="ml-4 lg:ml-8 mr-2 text-white">Name = </span>
+                <span className="text-gray-400">{`"`}</span>
                 <span className="text-amber-300">Sam Rodden</span>
-                <span className="text-gray-400">{`',`}</span>
+                <span className="text-gray-400">{`";`}</span>
               </div>
               <div className="ml-4 lg:ml-8 mr-2">
-                <span className=" text-white">skills:</span>
-                <span className="text-gray-400">{`['`}</span>
+                <span className="mr-2 text-pink-500">public string[] </span>
+                <span className=" text-white">mySkills =</span>
+                <span className="text-gray-400">{`{"`}</span>
                 <span className="text-amber-300">Unity</span>
-                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-gray-400">{'", "'}</span>
                 <span className="text-amber-300">C#</span>
-                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-gray-400">{'", "'}</span>
                 <span className="text-amber-300">VR/AR</span>
-                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-gray-400">{'", "'}</span>
                 <span className="text-amber-300">Visual Studios</span>
-                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-gray-400">{'", "'}</span>
                 <span className="text-amber-300">Quality Assurance</span>
-                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-gray-400">{'", "'}</span>
                 <span className="text-amber-300">Debugging</span>
-                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-gray-400">{'", "'}</span>
                 {/*<span className="text-amber-300">MongoDB</span>
-                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-gray-400">{'", "'}</span>
                 <span className="text-amber-300">Docker</span>
-                <span className="text-gray-400">{"', '"}</span>*/}
+                <span className="text-gray-400">{'", "'}</span>*/}
                 <span className="text-amber-300">Microsoft Office</span>
-                <span className="text-gray-400">{"'],"}</span>
+                <span className="text-gray-400">{'"};'}</span>
               </div>
               <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-white">hardWorker:</span>
+                <span className="ml-4 lg:ml-8 mr-2 text-pink-500">public bool </span>
+                <span className="text-white">hardWorker = </span>
                 <span className="text-orange-400">true</span>
-                <span className="text-gray-400">,</span>
+                <span className="text-gray-400">;</span>
               </div>
               <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-white">quickLearner:</span>
+              <span className="ml-4 lg:ml-8 mr-2 text-pink-500">public bool </span>
+                <span className="text-white">quickLearner = </span>
                 <span className="text-orange-400">true</span>
-                <span className="text-gray-400">,</span>
+                <span className="text-gray-400">;</span>
               </div>
               <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-white">problemSolver:</span>
+                `<span className="ml-4 lg:ml-8 mr-2 text-pink-500">public bool </span>
+                <span className="text-white">problemSolver = </span>
                 <span className="text-orange-400">true</span>
-                <span className="text-gray-400">,</span>
+                <span className="text-gray-400">;</span>
               </div>
               <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-green-400">hireable:</span>
-                <span className="text-orange-400">function</span>
+                <span className="ml-4 lg:ml-8 mr-2 text-green-400">public bool </span>
+                <span className="text-orange-400">Hireable</span>
                 <span className="text-gray-400">{'() {'}</span>
               </div>
               <div>
-                <span className="ml-8 lg:ml-16 mr-2 text-orange-400">return</span>
+                <span className="ml-8 lg:ml-16 mr-2 text-orange-400">return </span>
                 <span className="text-gray-400">{`(`}</span>
               </div>
               <div>
@@ -169,7 +174,7 @@ function HeroSection() {
               </div>
               <div>
                 <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
-                <span className="mr-2 text-white">skills.length</span>
+                <span className="mr-2 text-white">mySkills.Length</span>
                 <span className="mr-2 text-amber-300">&gt;=</span>
                 <span className="text-orange-400">5</span>
               </div>
