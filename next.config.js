1c29bb36c4cfe20ad061c7315fe70e101186f9a4
SamSamR <samroddenpro@gmail.com>
2024-04-11 15:50:19 -0400

    Configs Commit

--- next.config.js
+++ next.config.js
@@ -3,27 +3,17 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    domains: ['res.cloudinary.com', 'media.dev.to']
-  },
-
   basePath: "/developer-portfolio",  // <= name of project
   output: "export",  // <= enables static exports
   reactStrictMode: true,
 
-};
- 
-module.exports = nextConfig;
-
-/*module.exports = {
-
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
     domains: ['res.cloudinary.com', 'media.dev.to']
-  }
-}*/
+  },
+
+};
+ 
+module.exports = nextConfig;
