9c8b78ed44858d1d88fc4caf0a6e492a992437d2
SamSamR <samroddenpro@gmail.com>
2024-04-11 15:25:51 -0400

    Edited Config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
 const path = require('path')
 
- /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/developer-portfolio",  // <= name of project
-  output: "export",  // <= enables static exports
-  reactStrictMode: true,
 
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
     domains: ['res.cloudinary.com', 'media.dev.to']
-  }
+  },
+
+  basePath: "/developer-portfolio",  // <= name of project
+  output: "export",  // <= enables static exports
+  reactStrictMode: true,
 
 };
  
