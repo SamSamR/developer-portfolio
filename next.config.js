f97ab9cd22cabd048852aa4b0650a89d26465def
SamSamR <samroddenpro@gmail.com>
2024-04-11 12:38:11 -0400

    Testing new configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 const path = require('path')
+
+const nextConFig = {
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
  
 module.exports = {
+  //new addition
+  nextConFig,
+
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
@@ -8,3 +16,22 @@ module.exports = {
     domains: ['res.cloudinary.com', 'media.dev.to']
   }
 }
+
+//other way to try if above doesn't work
+/*
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    domains: ['res.cloudinary.com', 'media.dev.to']
+  }
+
+  //new addition
+  const nextConFig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  }
+  return nextConFig
+}
+*/
