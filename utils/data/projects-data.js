c7d44bc0f260c3148bda348746c7d97bb0169d65
SamSamR <samroddenpro@gmail.com>
2024-04-19 22:55:17 -0400

    Project Updates 3

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -7,7 +7,7 @@ export const projectsData = [
     {
         id: 1,
         name: 'VR Paper Airplanes',
-        description: 'Collaborated with a team to design and program a VR game in Unity, where players pick up & throws paper airplane into trash bins to score points. I created the level design, programmed the Game Manager & the airplane physics, point system, & UI. The game currently supports the Occulus headset.',
+        description: 'Collaborated with a team to design & program a VR game in Unity, where players pick up & throws paper airplane into trash bins to score points. I created the level design, programmed the Game Manager & the airplane physics, point system, & UI. The game currently supports the Oculus headset.',
         tools: ['Unity', 'Oculus Quest', "OpenXR", "C#"],
         role: 'Developer',
         code: '',
@@ -17,7 +17,7 @@ export const projectsData = [
     {
         id: 2,
         name: 'VR OSHA Violation Sim.',
-        description: 'A college project where I refactored & revitalized a legacy Unity VR project that would be used to train construction workers on various OSHA violations on jobsites. I updated & tested the level design to enhance efficiency & user experience & also updated original source code & scripts to be more efficient & scalable in the future. In addition, I created new menus & UI. This application currently supports the Occulus headset.',
+        description: 'A college project where I refactored & revitalized a legacy Unity VR application that could be used to train construction workers on various OSHA violations on sites. I updated & tested the level design to enhance efficiency, user experience, updated original scripts to be more efficient & scalable in the future & created new UI elements. This application currently supports the Oculus headset.',
         tools: ['Unity', 'Oculus Quest', 'OpenXR', 'C#'],
         role: 'Developer',
         code: '',
@@ -27,7 +27,7 @@ export const projectsData = [
     {
         id: 3,
         name: 'Castle Defender Prototype',
-        description: "A college project where we used custome netcode to create a multiplayer Game in Unity in 4 months. The goal of the game was for each player to build their castle, places down units, & defends their castle from other players. I was the system designer, artist, & programmer of this multiplayer strategy castle defender game. I programed the game mechanics, game economy, unit’s pathfinding AI, & the games UI. In addition, I also programmed the backend network for client/server communications & created most of the assets & animations.",
+        description: "A college project where we used custome netcode to create a multiplayer castle defender in Unity in 4 months. The goal of the game was for each player to build their castle, places down units, & defends their castle from other players. I was the system designer, artist, & programmer of this game. I programmed the game mechanics, game economy, unit’s pathfinding AI, & the server network for client/server communications. I also created the UI elements, 2D assets, & animations.",
         tools: ['Unity', 'C#', 'Aesprite', "Custom Netcode"],
         role: 'Developer, Pixel Artist, Animator',
         code: '',
@@ -37,7 +37,7 @@ export const projectsData = [
     {
         id: 4,
         name: 'Space Game Prototype',
-        description: "A coolege project where I designed & programmed a mockup/prototype single-player 3D space game in Unity. Developed puzzle mechanics where the player must use their position & speed to defeat giant enemies. Created the UI & graphic design, sound design, & some of the 3D assets in Maya.	I also created & programed custom shaders in Unity.",
+        description: "A college project where I designed & prototyped a single-player 3D space game in Unity. Developed puzzle mechanics where the player must use their velocity to defeat giant enemies. Created the UI elements, sounds, & 3D models in Maya.",
         tools: ['Unity', 'Shader Graphs', 'Blender', '####Music/sound Program###'],
         role: 'Developer, Artist, Sound/Music Designer',
         code: '',
